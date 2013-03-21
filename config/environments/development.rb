62f7419d2c4cbd51c9940dfe58404d1d2ca610ca
Lionel <nenebale@gmail.com>
2013-03-21 19:05:37 +0100

    Revert "SendGrid configs hinzugefügt"
    This reverts commit 20629b1a7b729c7ab8a8ece484a6fa9f47a922ba.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,5 @@
 WebcakeService::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
