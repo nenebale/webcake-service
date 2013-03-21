5558434abde892f5cceb30d3c8f6bde227828027
Lionel <nenebale@gmail.com>
2013-03-21 19:06:51 +0100

    Mailgun configs hinzugefügt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,15 @@
 WebcakeService::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
