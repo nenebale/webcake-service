cb1712a7f144cb9376e442e5bb08a47f540048e2
Lionel <nenebale@gmail.com>
2013-03-21 17:24:17 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebcakeService::Application.config.secret_token = 'a915d3b061a9be310c82007ca0d2342672a5b8b740b9632a62d5aacd23f366f2e1278bd49f4091f8ed46c09b752eeb8132cef832b2bc73128d2e61b5e1a47d84'
---
+++ unicorn.rb
@@ -0,0 +1,2 @@
+worker_processes 4 # Definiert wie viele workers von Unicorn gestartet werden
+timeout 30 # und nach welcher Zeit diese beendet werden, sollte der Prozess abstürzen
