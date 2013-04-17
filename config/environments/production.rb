40e803de254fc4af7e56848758512f639d2301e1
Lionel <nenebale@gmail.com>
2013-04-17 18:43:06 +0200

    production.rb aufgeräumt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,34 +1,16 @@
 WebcakeService::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  # ActionMailer::Base.smtp_settings = {
-  #   :port           => ENV['MAILGUN_SMTP_PORT'], 
-  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
-  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  #   :domain         => 'yourapp.heroku.com',
-  #   :authentication => :plain,
-  # }
-  # ActionMailer::Base.smtp_settings = {
-#     :tls => true,
-#     :address => "smtp.gmail.com",
-#     :port => "587",
-#     :domain => "webcake.ch",
-#     :authentication => :plain,
-#     :user_name => ENV['GAMIL_USERNAME'],
-#     :password => ENV['GAMIL_PASSWORD']
-#   }
-#   ActionMailer::Base.delivery_method = :smtp
-
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'webcake.ch',
-  user_name:            ENV['GMAIL_USERNAME'],
-  password:             ENV['GMAIL_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:  'smtp.gmail.com',
+    port: 587,
+    domain: 'webcake.ch',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests
   config.cache_classes = true
