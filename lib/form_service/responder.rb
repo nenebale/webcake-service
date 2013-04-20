6367008de8edbd9da2066cb84402c6cfaea4fdb7
Lionel <nenebale@gmail.com>
2013-04-20 16:06:58 +0200

    Module FormService umbennant zu Service

--- lib/form_service/responder.rb
+++
@@ -1,5 +0,0 @@
-module FormService::Responder
-  def post_response(response = {:type => :text, :data => '', :status => '200'})
-    render response[:type] => response[:data], :status => response[:status]
-  end
-end
