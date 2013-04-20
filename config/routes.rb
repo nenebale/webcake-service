01730b63dd5359580d8aa419d8b4adae55e9f0e3
Lionel <nenebale@gmail.com>
2013-04-20 16:05:19 +0200

    Routes sinnvoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WebcakeService::Application.routes.draw do
-  resources :customers
+  resources :customers do
+    resources :websites
+  end
   resources :emails
   match '/' => redirect("http://www.webcake.ch")
 end
