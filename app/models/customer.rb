547f964212da2f0afab614280adbafa56579b50f
Lionel <nenebale@gmail.com>
2013-04-20 16:04:40 +0200

    Customer Model mit Validations ergänzt

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ActiveRecord::Base
-  validates :uuid, :email, :uniqueness => true
+  
+  has_many :websites
+  
   validates :uuid, :email, :presence => true
+  validates :uuid, :email, :uniqueness => true
+  validates_format_of :email, :with => /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+  
 end
